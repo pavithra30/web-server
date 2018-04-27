2b26c52f536c77de76eb4d38f9322c3d2c589473
Pavithra Thimmappa <EVOLVUS\pavithrat@EPCBLR-035.evolvus.com>
2018-04-27 17:23:13 +0530

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express=require('express');
 const hbs=require('hbs');
+
+const port=process.env.PORT || 3000;
 var app=express();
 hbs.registerPartials(__dirname+'/views/partials')
 hbs.registerHelper('getCurrentYear',()=>{
@@ -23,4 +25,4 @@ app.get('/',(req,res)=>{
    });
  });
 
- app.listen(3000);
+ app.listen(port);
